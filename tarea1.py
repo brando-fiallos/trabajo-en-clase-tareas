606efa332bcbe7f69e67f9116757862dfc434012
brando-fiallos <fiallosbrando57@gmail.com>
2025-05-29 20:32:32 -0500

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,12 @@
+x = 5 
+print('hola')
+
+def print_lyrics():
+    print("soy un leñador, y estoy bien.")
+    print('duermo toda la noche y trabajo todo el dia.')
+
+print('yo')
+print_lyrics()
+x = x + 2 
+print (x)
+
